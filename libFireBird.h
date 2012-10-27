c9d7dce8a03fe9b9cc0d42232072569a8cdacff5
firebird <firebird@262b626a-60eb-0b49-a07d-6d01a670bd41>
2012-10-27 19:22:14 +0000

    * Fixed: FixInvalidFileName() is now UTF8 aware. Use this function to remove unwanted characters from a file name

--- libFireBird.h
+++ libFireBird.h
@@ -3,7 +3,7 @@
 
 //  #define DEBUG_FIREBIRDLIB
 
-  #define __FBLIB_RELEASEDATE__ "2012-10-19"
+  #define __FBLIB_RELEASEDATE__ "2012-10-27"
 
   #ifdef _TMSEMU_
     #define __FBLIB_VERSION__ __FBLIB_RELEASEDATE__" TMSEmulator"
