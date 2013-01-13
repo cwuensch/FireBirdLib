6e257e8e0b2bf41468bea7593142354e82a2361a
firebird <firebird@262b626a-60eb-0b49-a07d-6d01a670bd41>
2013-01-13 14:11:48 +0000

    * Fixed: made FixInvalidFileName(), ReplaceInvalidFileName() and ValidFileName() UTF8 aware

--- libFireBird.h
+++ libFireBird.h
@@ -3,7 +3,7 @@
 
 //  #define DEBUG_FIREBIRDLIB
 
-  #define __FBLIB_RELEASEDATE__ "2012-12-25"
+  #define __FBLIB_RELEASEDATE__ "2013-01-13"
 
   #ifdef _TMSEMU_
     #define __FBLIB_VERSION__ __FBLIB_RELEASEDATE__" TMSEmulator"
