914d20875047db29bc468793fcadafc1e77bd44b
firebird <firebird@262b626a-60eb-0b49-a07d-6d01a670bd41>
2016-11-30 18:52:49 +0000

    * Fixed: PlayMediaFile() crashed with .avi and .divx files

--- av/PlayMediaFile.c
+++ av/PlayMediaFile.c
@@ -29,22 +29,18 @@ bool PlayMediaFile(char *MediaFileName)
 
     if((access(tempINF, F_OK) != -1) && HDD_isRecFileName(MediaFileName))
       ret = Appl_StartPlayback(TempFileName, 0, TRUE, FALSE) == 0;
+    else if(StringEndsWith(TempFileName, ".avi") || StringEndsWith(TempFileName, ".divx"))
+    {
+      ret = Appl_StartPlaybackDivx(TempFileName, 0, FALSE) == 0;
+    }
+    else if(StringEndsWith(TempFileName, ".mp3"))
+    {
+      ret = Appl_StartPlaybackMp3(TempFileName) == 0;
+    }
     else
     {
-      if(StringEndsWith(TempFileName, ".avi") || StringEndsWith(TempFileName, ".divx"))
-      {
-        ret = Appl_StartPlaybackDivx(TempFileName, 0, FALSE) == 0;
-      }
-      if(StringEndsWith(TempFileName, ".mp3"))
-      {
-        ret = Appl_StartPlaybackMp3(TempFileName) == 0;
-      }
-      else
-      {
-        ret = Appl_StartPlaybackMedia(TempFileName, 0, TRUE, FALSE) == 0;
-      }
+      ret = Appl_StartPlaybackMedia(TempFileName, 0, TRUE, FALSE) == 0;
     }
-
   }
   ApplHdd_RestoreWorkFolder();
 
--- libFireBird.h
+++ libFireBird.h
@@ -3,7 +3,7 @@
 
   //#define STACKTRACE
 
-  #define __FBLIB_RELEASEDATE__ "2016-11-22"
+  #define __FBLIB_RELEASEDATE__ "2016-11-30"
 
   #define __FBLIB_VERSION__ __FBLIB_RELEASEDATE__
 
