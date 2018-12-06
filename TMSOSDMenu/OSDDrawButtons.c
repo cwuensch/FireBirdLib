b7459dd76f841736c8665b0aba6b5b347e626a62
ibbi <ibbi@262b626a-60eb-0b49-a07d-6d01a670bd41>
2018-12-06 17:58:11 +0000

    * Fixed: Limit button drawing to the end of the separation line (minus margin)

--- TMSOSDMenu/OSDDrawButtons.c
+++ TMSOSDMenu/OSDDrawButtons.c
@@ -12,7 +12,7 @@ void OSDDrawButtons(void)
   for(i = 0; i < pMenu->NrButtons; i++)
   {
     TAP_Osd_PutGd(OSDRgn, pMenu->Buttons[i].X , pMenu->Buttons[i].Y, pMenu->Buttons[i].pButtonGd, TRUE);
-    FMUC_PutStringAA(OSDRgn, pMenu->Buttons[i].X + pMenu->Buttons[i].pButtonGd->width + 2, pMenu->Buttons[i].Y + 3 + FONTYOFFSET, 670, pMenu->Buttons[i].Text, pMenu->Buttons[i].Color, COLOR_None, pMenu->FontButtons, FALSE, ALIGN_LEFT, 1);
+    FMUC_PutStringAA(OSDRgn, pMenu->Buttons[i].X + pMenu->Buttons[i].pButtonGd->width + 2, pMenu->Buttons[i].Y + 3 + FONTYOFFSET, 658, pMenu->Buttons[i].Text, pMenu->Buttons[i].Color, COLOR_None, pMenu->FontButtons, FALSE, ALIGN_LEFT, 1);
   }
 
   if(CallbackProcedure) CallbackProcedure(OSDCB_Buttons, OSDRgn);
