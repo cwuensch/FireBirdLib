26a4aa352b598d3c502ec4d312753d2e1111ec1b
ibbi <ibbi@262b626a-60eb-0b49-a07d-6d01a670bd41>
2018-12-06 17:59:46 +0000

    * Fixed: OSDMenu's left title was 3 px too far to the right and 1 px too low (right title was 3 px too low)

--- TMSOSDMenu/OSDDrawTitle.c
+++ TMSOSDMenu/OSDDrawTitle.c
@@ -9,10 +9,10 @@ void OSDDrawTitle(void)
 
   pMenu = &Menu[CurrentMenuLevel];
 
-  FMUC_PutStringAA(OSDRgn, 53, 40 + FONTYOFFSET, 658, Menu[CurrentMenuLevel].TitleLeft, RGB(230,230,250), COLOR_None, pMenu->FontLeftTitle, FALSE, ALIGN_LEFT, 1);
+  FMUC_PutStringAA(OSDRgn, 50, 39 + FONTYOFFSET, 658, Menu[CurrentMenuLevel].TitleLeft, RGB(230,230,250), COLOR_None, pMenu->FontLeftTitle, FALSE, ALIGN_LEFT, 1);
   tw = FMUC_GetStringWidth(Menu[CurrentMenuLevel].TitleLeft, pMenu->FontLeftTitle);
 
-  FMUC_PutStringAA(OSDRgn, 53 + tw + 5, 52 + FONTYOFFSET, 658, Menu[CurrentMenuLevel].TitleRight, RGB(102,102,102), COLOR_None, pMenu->FontRightTitle, TRUE, ALIGN_RIGHT, 1);
+  FMUC_PutStringAA(OSDRgn, 50 + tw + 5, 49 + FONTYOFFSET, 658, Menu[CurrentMenuLevel].TitleRight, RGB(102,102,102), COLOR_None, pMenu->FontRightTitle, TRUE, ALIGN_RIGHT, 1);
 
   if(CallbackProcedure) CallbackProcedure(OSDCB_Title, OSDRgn);
 
