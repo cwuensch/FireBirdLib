4e29c30ebc7b03f2397bec7c25517cb8a6cb04df
Christian Wünsch <23318276+cwuensch@users.noreply.github.com>
2019-01-11 20:22:02 +0000

    Reimplementierung von LangLoadStrings (sehr viel effizienter)

--- ini/LangLoadStrings.c
+++ ini/LangLoadStrings.c
@@ -1,115 +1,89 @@
+#define _GNU_SOURCE
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "FBLib_ini.h"
 #include "libFireBird.h"
 
 dword                   LangNrStrings = 0;
-dword                   *LangStringPtr = NULL;
-char                    *LangStrings   = NULL;
+dword                  *LangStringPtr = NULL;
+char                   *LangStrings   = NULL;
 
-INILOCATION LangLoadStrings(char *LangFile, dword NrStrings, int FallbackLang, char *AppName)
+int LangLoadStrings(char *AbsLangFile, dword NrStrings, int FallbackLang)
 {
-  TRACEENTER();
+  FILE                 *f = NULL;
+  char                 *OSDLang = iso639_1 (TAP_GetSystemVar(SYSVAR_OsdLan));
+  char                 *SecLang = iso639_1 (FallbackLang);
+  char                 *LangBuffer = NULL, *Buffer = NULL;
+  size_t                BufSize = 0;
+  char                 *c = NULL;
+  char                  CurLang[3], CurText[200];
+  dword                 CurNr, TotalLength;
 
-  dword                 i;
-  int                   OSDLan;
-  char                  Key[10], s[512], *c;
-  dword                 TotalLength;
-  char                  *OldBuffer, *OldCommentBuffer;
-  dword                 OldBufferSize;
-  INILOCATION           LangLocation;
+  TRACEENTER();
 
   if(LangStringPtr || LangStrings) LangUnloadStrings();
 
-  OSDLan = TAP_GetSystemVar(SYSVAR_OsdLan);
-  if((NrStrings > 999999) || !iso639_1(OSDLan) || (FallbackLang > LAN_Bulgarian))
+  if((NrStrings > 999999) || !OSDLang || (FallbackLang > LAN_Bulgarian))
   {
     TRACEEXIT();
-    return INILOCATION_NotFound;
+    return FALSE;
   }
 
-  OldBuffer = INIBuffer;
-  OldCommentBuffer = INICommentBuffer;
-  OldBufferSize = BufferSize;
-  HDD_TAP_PushDir();
-  if((LangLocation = INIOpenFile(LangFile, AppName)) < INILOCATION_AtCurrentDir)
-  {
-    if(LangLocation == INILOCATION_NewFile) INICloseFile();
-    HDD_TAP_PopDir();
-
-    TRACEEXIT();
-    return INILOCATION_NotFound;
-  }
-
-  //First loop: count the length of all strings
+  TotalLength = 1;
+  LangNrStrings = 0;
+  LangBuffer = TAP_MemAlloc(NrStrings * 200 + 1);
   LangStringPtr = TAP_MemAlloc(NrStrings * sizeof(dword));
+  memset(LangBuffer, 0, NrStrings * 200 + 1);
   memset(LangStringPtr, 0, NrStrings * sizeof(dword));
-  TotalLength = 0;
-  for(i = 0; i < NrStrings; i++)
+
+  if ((f = fopen(AbsLangFile, "rb")) != NULL)
   {
-    LangStringPtr[i] = TotalLength;
-    TAP_SPrint(Key, "%s_%3.3d", iso639_1(OSDLan), i);
-    INIGetString(Key, s, "", sizeof(s));
-
-    //If the string is not available in the current language, use the fallback language
-    if(!s[0] && (FallbackLang >= 0))
+    while (getline(&Buffer, &BufSize, f) >= 0)
     {
-      TAP_SPrint(Key, "%s_%3.3d", iso639_1(FallbackLang), i);
-      INIGetString(Key, s, "", sizeof(s));
-    }
+      // Remove line breaks in the end
+/*      p = strlen(Buffer);
+      while (p && (Buffer[p-1] == '\r' || Buffer[p-1] == '\n'))
+        Buffer[--p] = '\0';
 
-    //Convert control characters
-    c = s;
-    while((c = strchr(c, '\\')))
-    {
-      if(c[1] == 'r') *c ='\r';
-      else if(c[1] == 'n') *c ='\n';
-      else continue;
-
-      memmove(c + 1, c + 2, strchr(c, '\0') - c - 1);
+      // Kommentare und Sektionen
+      switch (Buffer[0])
+      {
+        case '\0':
+        case '%':
+        case ';':
+        case '#':
+        case '/':
+          continue;
+      }
+*/
+      if (sscanf(Buffer, "%2[a-z]_%lu = %199[^\r\n]", CurLang, &CurNr, CurText) == 3)
+      {
+        if (CurNr < NrStrings)
+        {
+          if ((strncmp(CurLang, OSDLang, 2) == 0) || (!LangStringPtr[CurNr] && strncmp(CurLang, SecLang, 2) == 0))
+          {
+            c = CurText;
+            while((c = strchr(c, '\\')))
+            {
+              if(c[1] == 'r') *c ='\r';
+              else if(c[1] == 'n') *c ='\n';
+              else continue;
+              memmove(c + 1, c + 2, strchr(c, '\0') - c - 1);
+            }
+            strcpy(&LangBuffer[TotalLength], CurText);
+            LangStringPtr[CurNr] = TotalLength;
+            TotalLength += (strlen(CurText) + 1);
+          }
+        }
+      }
     }
-    TotalLength += (strlen(s) + 1);
+    fclose(f);
+    LangStrings = TAP_MemAlloc(TotalLength);
+    memcpy(LangStrings, LangBuffer, TotalLength);
+    LangNrStrings = NrStrings;
   }
-
-  //Second loop: Read the strings
-  LangStrings = TAP_MemAlloc(TotalLength);
-  memset(LangStrings, 0, TotalLength);
-  for(i = 0; i < NrStrings; i++)
-  {
-    TAP_SPrint(Key, "%s_%3.3d", iso639_1(OSDLan), i);
-    INIGetString(Key, s, "", sizeof(s));
-
-    //If the string is not available in the current language, use the fallback language
-    if(!s[0] && (FallbackLang >= 0))
-    {
-      TAP_SPrint(Key, "%s_%3.3d", iso639_1(FallbackLang), i);
-      INIGetString(Key, s, "", sizeof(s));
-    }
-
-    //Convert control characters
-    c = s;
-    while((c = strchr(c, '\\')))
-    {
-      if(c[1] == 'r') *c ='\r';
-      else if(c[1] == 'n') *c ='\n';
-      else continue;
-
-      memmove(c + 1, c + 2, strchr(c, '\0') - c - 1);
-    }
-
-    strcpy(&LangStrings[LangStringPtr[i]], s);
-  }
-
-  INICloseFile();
-
-  INIBuffer = OldBuffer;
-  INICommentBuffer = OldCommentBuffer;
-  BufferSize = OldBufferSize;
-
-  LangNrStrings = NrStrings;
-
-  HDD_TAP_PopDir();
 
   TRACEEXIT();
-  return LangLocation;
+  return LangNrStrings;
 }
