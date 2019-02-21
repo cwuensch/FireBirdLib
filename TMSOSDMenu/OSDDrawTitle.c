aee89b7b2febf124dbf09d86b63859d3470ec1c7
ibbi <ibbi@262b626a-60eb-0b49-a07d-6d01a670bd41>
2019-02-21 15:45:35 +0000

    * Fixed: The title area has never been erased if marked dirty

--- TMSOSDMenu/OSDDrawTitle.c
+++ TMSOSDMenu/OSDDrawTitle.c
@@ -7,6 +7,8 @@ void OSDDrawTitle(void)
   dword                 tw;
   tMenu                *pMenu;
 
+  TAP_Osd_FillBox(OSDRgn, 0, 0, 720, 96, RGB(16, 16, 16));
+
   pMenu = &Menu[CurrentMenuLevel];
 
   FMUC_PutStringAA(OSDRgn, 50, 39 + FONTYOFFSET, 658, Menu[CurrentMenuLevel].TitleLeft, RGB(230,230,250), COLOR_None, pMenu->FontLeftTitle, FALSE, ALIGN_LEFT, 1);
