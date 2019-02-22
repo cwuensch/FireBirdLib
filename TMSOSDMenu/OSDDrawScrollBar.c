9141935150fbbb748d15cf8bd942c8956fb075d8
ibbi <ibbi@262b626a-60eb-0b49-a07d-6d01a670bd41>
2019-02-22 13:27:10 +0000

    Internal: Make OSDDrawScrollBar() easier to read by adding pMenu

--- TMSOSDMenu/OSDDrawScrollBar.c
+++ TMSOSDMenu/OSDDrawScrollBar.c
@@ -4,17 +4,20 @@ void OSDDrawScrollBar(void)
 {
   TRACEENTER();
 
+  tMenu                *pMenu;
   int                   n, Y;
 
-  if(Menu[CurrentMenuLevel].NrItems < Menu[CurrentMenuLevel].NrLines + 1)
+  pMenu = &Menu[CurrentMenuLevel];
+
+  if(pMenu->NrItems < pMenu->NrLines + 1)
   {
     TAP_Osd_PutGd(OSDRgn, 661, 96, &_ScrollBarInvisible_Gd, FALSE);
   }
   else
   {
     TAP_Osd_PutGd(OSDRgn, 661, 96, &_ScrollBarVisible_Gd, FALSE);
-    n = (Menu[CurrentMenuLevel].OSDMenuDisplayMode == OMDM_Text ? Menu[CurrentMenuLevel].NrLines : 1);
-    Y = 108 + (Menu[CurrentMenuLevel].CurrentSelection * 324) / (Menu[CurrentMenuLevel].NrItems - n);
+    n = (pMenu->OSDMenuDisplayMode == OMDM_Text ? pMenu->NrLines : 1);
+    Y = 108 + (pMenu->CurrentSelection * 324) / (pMenu->NrItems - n);
     TAP_Osd_PutGd(OSDRgn, 662, Y, &_ScrollBarKnob_Gd, FALSE);
   }
 
